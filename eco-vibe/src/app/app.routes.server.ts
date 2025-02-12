07da05dbdd4370e763fee017e4f86015309cac8f
narciss98 <narciss98@gmail.com>
2025-02-12 16:17:25 +0100

    Route changes

--- eco-vibe/src/app/app.routes.server.ts
+++ eco-vibe/src/app/app.routes.server.ts
@@ -1,6 +1,4 @@
 import { RenderMode, ServerRoute, PrerenderFallback } from '@angular/ssr';
-import { inject } from '@angular/core';
-import { PostService } from './components/forum/services/post.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
@@ -26,19 +24,6 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'contact',
     renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'post/:id',
-    renderMode: RenderMode.Prerender,
-    getPrerenderParams: async () => {
-      const postService = inject(PostService); 
-      const ids = await postService.getAllPostsIds().toPromise(); 
-      if (!ids || ids.length === 0) {
-        console.error('No post IDs found for prerendering.');
-        return []; 
-      }
-      return ids.map(id => ({ id }));
-    },
   },
   {
     path: 'donate',
